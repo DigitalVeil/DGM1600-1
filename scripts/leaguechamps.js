ce6f1209ab7ecca1e52d60c0f7996481f6875d45
Timothy Perez <tcperez1993@gmail.com>
2019-04-10 00:01:54 -0600

    Added Create New Champion Button

--- scripts/leaguechamps.js
+++ scripts/leaguechamps.js
@@ -1,26 +1,49 @@
 import { champs } from '../data/champs.js'
 
-console.log(champs)
+class Champion {
+    constructor(name, title){
+        this.name = name
+        this.title = title
+    }
+}
+
+
+
+
+
+
+
 const mainContainer = document.querySelector('.container')
 mainContainer.className = 'container'
+
+
 
 champs.forEach((champs) => { 
 let card = document.createElement('div')
     card.className = 'box'
+    let figure = document.createElement('figure')
     let name = document.createElement('figcaption')
     let image = document.createElement('img')
     let title = document.createElement('p')
-    
+    let newChampions = new Champion('Zoe', 'The Aspect of Twilight')
     
     name.textContent = champs.name
     title.textContent = champs.title
     image.src = champs.url
-    card.appendChild(image)
-    card.appendChild(name)
-    card.appendChild(title)
+    figure.appendChild(image)
+    figure.appendChild(name)
+    figure.appendChild(title)
+    card.appendChild(figure)
     mainContainer.appendChild(card)
-  
+});  
 
+console.log(newChampions)
+  const newChampionButton = document.querySelector('button')
+
+    newChampionButton.addEventListener('click', function(){
+       
+});
+    
 // function createLeagueCard(champdata) {
     
 // }
@@ -29,4 +52,3 @@ let card = document.createElement('div')
 // //         console.log(myJson)
 // //         createLeagueCard(myJson)
 // //     });
-})
