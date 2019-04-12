f022a4b3e0924b6d05802ff3c90597b9929cf495
Timothy Perez <tcperez1993@gmail.com>
2019-04-11 22:09:46 -0600

    Added Styling/Flip 1.0 version done

--- data/champs.js
+++ data/champs.js
@@ -3,156 +3,182 @@ export const champs = [
         "name": "Aatrox",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_7.jpg",
         "difficulty": 4,
-        "title": "The Darkin Blade"
+        "title": "The Darkin Blade",
+        "class": "Juggernaut"
     },
     {
         "name": "Braum",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Braum_0.jpg",
         "difficulty": 3,
-        "title": "The Heart of the Freljord"
+        "title": "The Heart of the Freljord",
+        "class": "Warden"
     },
     {
         "name": "Caitlyn",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Caitlyn_11.jpg",
         "difficulty": 6,
-        "title": "The Sheriff of Piltover"
+        "title": "The Sheriff of Piltover",
+        "class": "Marksman"
     }, 
     {
         "name": "Diana",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Diana_11.jpg",
         "difficulty": 4,
-        "title": "Scorn of the Moon"
+        "title": "Scorn of the Moon",
+        "class": "Diver"
     },
     {
         "name": "Ekko",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Ekko_0.jpg",
         "difficulty": 8,
-        "title": "The Boy Who Shattered Time"
+        "title": "The Boy Who Shattered Time",
+        "class": "Assassin"
     },
     {
         "name": "Fiora",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Fiora_22.jpg",
         "difficulty": 3,
-        "title": "The Grand Duelist"
+        "title": "The Grand Duelist",
+        "class": "Skirmisher"
     },
     {
         "name": "Galio",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Galio_0.jpg",
         "difficulty": 5,
-        "title": "The Colossus"
+        "title": "The Colossus",
+        "class": "Warden"
     },
     {
         "name": "Heimerdinger",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Heimerdinger_0.jpg",
         "difficulty": 8,
-        "title": "The Revered Inventor"
+        "title": "The Revered Inventor",
+        "class": "Specialist"
     },
     {
         "name": "Irelia",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Irelia_6.jpg",
         "difficulty": 5,
-        "title": "The Blade Dancer"
+        "title": "The Blade Dancer",
+        "class": "Diver"
     },
     {
         "name": "Jhin",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Jhin_4.jpg",
         "difficulty": 6,
-        "title": "The Virtuoso"
+        "title": "The Virtuoso",
+        "class": "Marksman"
     },
     {
         "name": "Kai'sa",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Kaisa_0.jpg",
         "difficulty": 6,
-        "title": "Daughter of the Void"
+        "title": "Daughter of the Void",
+        "class": "Marksman"
     },
     {
         "name": "Lucian",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Lucian_8.jpg",
         "difficulty": 6,
-        "title": "The Purifier"
+        "title": "The Purifier",
+        "class": "Marksman"
     },
     {
         "name": "Morgana",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Morgana_0.jpg",
         "difficulty": 1,
-        "title": "The Fallen"
+        "title": "The Fallen",
+        "class": "Catcher"
     },
     {
         "name": "Nunu",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Nunu_0.jpg",
         "difficulty": 4,
-        "title": "The Boy and His Yeti"
+        "title": "The Boy and His Yeti",
+        "class": "Warden"
     },
     {
         "name": "Ornn",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Ornn_0.jpg",
         "difficulty": 5,
-        "title": "The Fire below the Mountain"
+        "title": "The Fire below the Mountain",
+        "class": "Vanguard"
     },
     {
         "name": "Poppy",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Poppy_5.jpg",
         "difficulty": 6,
-        "title": "Keeper of the Hammer"
+        "title": "Keeper of the Hammer",
+        "class": "Warden"
     },
     {
         "name": "Quinn",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Quinn_2.jpg",
         "difficulty": 5,
-        "title": "Demacia's Wings"
+        "title": "Demacia's Wings",
+        "class": "Specialist"
     },
     {
         "name": "Rakan",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Rakan_0.jpg",
         "difficulty": 5,
-        "title": "The Charmer"
+        "title": "The Charmer",
+        "class": "Catcher"
     },
     {
         "name": "Swain",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Swain_4.jpg",
         "difficulty": 8,
-        "title": "The Noxian Grand General"
+        "title": "The Noxian Grand General",
+        "class": "Battlemage"
     },
     {
         "name": "Thresh",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Thresh_5.jpg",
         "difficulty": 7,
-        "title": "The Chain Warden"
+        "title": "The Chain Warden",
+        "class": "Catcher"
     },
     {
         "name": "Urgot",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Urgot_0.jpg",
         "difficulty": 8,
-        "title": "The Dreadnought"
+        "title": "The Dreadnought",
+        "class": "Juggernaut"
     },
     {
         "name": "Vayne",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Vayne_11.jpg",
         "difficulty": 8,
-        "title": "The Night Hunter"
+        "title": "The Night Hunter",
+        "class": "Marksman"
     },
     {
         "name": "Warwick",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Warwick_0.jpg",
         "difficulty": 3,
-        "title": "The Uncaged Wrath of Zaun"
+        "title": "The Uncaged Wrath of Zaun",
+        "class": "Diver"
     },
     {
         "name": "Xerath",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Xerath_0.jpg",
         "difficulty": 8,
-        "title": "The Magus Ascendant"
+        "title": "The Magus Ascendant",
+        "class": "Artillery"
     },
     {
         "name": "Yasuo",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Yasuo_9.jpg",
         "difficulty": 10,
-        "title": "The Unforgiven"
+        "title": "The Unforgiven",
+        "class": "Skirmisher"
     },
     {
         "name": "Zed",
         "url": "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Zed_10.jpg",
         "difficulty": 7,
-        "title": "The Master of Shadows"
+        "title": "The Master of Shadows",
+        "class": "Assassin"
     }
 ]
--- scripts/leaguechamps.js
+++ scripts/leaguechamps.js
@@ -1,5 +1,8 @@
 import { champs } from '../data/champs.js'
 
+const mainContainer = document.querySelector('.container')
+mainContainer.className = 'container'
+
 class Champion {
     constructor(name, title){
         this.name = name
@@ -7,43 +10,109 @@ class Champion {
     }
 }
 
+let newChampions = new Champion('Zoe', 'The Aspect of Twilight')
 
-
-
-
-
-
-const mainContainer = document.querySelector('.container')
-mainContainer.className = 'container'
-
-
-
-champs.forEach((champs) => { 
-let card = document.createElement('div')
-    card.className = 'box'
+function cardFront(champs){
+    let cardFront = document.createElement('div')
+    cardFront.className = 'card__face'
     let figure = document.createElement('figure')
     let name = document.createElement('figcaption')
     let image = document.createElement('img')
     let title = document.createElement('p')
-    let newChampions = new Champion('Zoe', 'The Aspect of Twilight')
-    
+   
     name.textContent = champs.name
     title.textContent = champs.title
     image.src = champs.url
     figure.appendChild(image)
     figure.appendChild(name)
     figure.appendChild(title)
-    card.appendChild(figure)
-    mainContainer.appendChild(card)
+    cardFront.appendChild(figure)
+    return cardFront
+}
+
+function cardBackInfo(champs){
+    let infoDiv = document.createElement('div')
+    let champClass = document.createElement('p')
+    let champDifficulty = document.createElement('p')
+    let champRarity = document.createElement('p')
+
+    champClass.textContent = champs.class
+    champDifficulty.textContent = "Difficulty :"
+    champRarity.textContent = ("Difficulty: ", + champs.difficulty)
+
+    
+    infoDiv.appendChild(champClass)
+    infoDiv.appendChild(champDifficulty)
+    infoDiv.appendChild(champRarity)
+    return infoDiv
+}
+
+function cardBack(champs){
+    let cardBack = document.createElement('div')
+    let backImage = document.createElement('img')
+    backImage.src = `../jpg/League.png`
+    cardBack.className = 'card__face card__face--back'
+
+    cardBack.appendChild(backImage)
+    cardBack.appendChild(cardBackInfo(champs))
+    return cardBack
+}
+
+function newBack(Champion){
+    let newBack = document.createElement('div')
+    let backImage = document.createElement('img')
+    backImage.src = `../jpg/League.png`
+    cardBack.className = 'card__face card__face--back'
+
+    newBack.appendChild(backImage)
+    cardBack.appendChild(cardBackInfo(champs))
+    return cardBack
+}
+
+champs.forEach((champs) => { 
+    let scene = document.createElement('div')
+    scene.className = 'scene'
+    let card = document.createElement('div')
+    card.className = 'card'
+
+    card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+
+    card.appendChild(cardFront(champs))
+    card.appendChild(cardBack(champs))
+    scene.appendChild(card)
+    mainContainer.appendChild(scene)
 });  
 
-console.log(newChampions)
+function createNewChampion(Champion){
+    let card = document.createElement('div')
+    card.className = 'box'
+    let figure = document.createElement('figure')
+    let name = document.createElement('figcaption')
+    let image = document.createElement('img')
+    let title = document.createElement('p')
+
+    card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+   
+    name.textContent = Champion.name
+    title.textContent = Champion.title
+    image.src = `../jpg/League.png`
+    figure.appendChild(image)
+    figure.appendChild(name)
+    figure.appendChild(title)
+    card.appendChild(figure)
+    mainContainer.appendChild(card)
+}
   const newChampionButton = document.querySelector('button')
 
     newChampionButton.addEventListener('click', function(){
-       
+        let newChampion = prompt('Enter Name of your champion!')
+       createNewChampion(new Champion('Zoe', 'The Aspect of Twilight'))
 });
-    
+
 // function createLeagueCard(champdata) {
     
 // }
@@ -51,4 +120,18 @@ console.log(newChampions)
 // //     .then(function(myJson) {
 // //         console.log(myJson)
 // //         createLeagueCard(myJson)
-// //     });
+// //     }); 
+
+// let figure = document.createElement('figure')
+    // let name = document.createElement('figcaption')
+    // let image = document.createElement('img')
+    // let title = document.createElement('p')
+   
+    
+    // name.textContent = champs.name
+    // title.textContent = champs.title
+    // image.src = champs.url
+    // figure.appendChild(image)
+    // figure.appendChild(name)
+    // figure.appendChild(title)
+    // card.appendChild(figure)
